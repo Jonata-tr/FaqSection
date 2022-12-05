effe5258419e05196f71cfecfd14e12020c663c9
Jonatan Trindade <109476031+Jonata-tr@users.noreply.github.com>
2022-12-04 20:25:30 -0400

    Finalizado o funcionamento do Faq
    Falta apenas estilizar

--- src/faqSection/acordion/faqContent.js
+++ src/faqSection/acordion/faqContent.js
@@ -1,13 +1,57 @@
-export const ShowAnswers = ({heading, content}) => {
+import {useState} from 'react'
+
+export const ShowAnswers = () => {
+
+  const [click, setClick] = useState(null)
+
+  const alternarRespostas = (i) => {
+    if(click === i){
+      return setClick(null)
+    }    
+
+    setClick(i)
+  }
+
   return(
-    <div className="perguntas" onClick={() => {
-      const show = document.querySelector(".respostas")
-      show.classList.toggle("hide")
-    }}>
-      <span>{heading}</span>
-      <div className="respostas hide">
-        {content}
-      </div>
+    <div className='faqs'>
+      {acordionContent.map((valor, index) => (
+          <>
+          <div className='perguntas' onClick={() => {setClick(index)}}>
+            {valor.heading}
+          </div>
+          <div className={click === index ? "respostas show" : 'respostas'}>
+            {valor.content}
+          </div>
+          </>
+      ))}
     </div>
   )
 }
+
+const acordionContent = [
+  {
+    id:'1',
+    heading: 'How many team members can I invite?',
+    content: 'You can invite up to 2 additional users on the Free plan. There is no limit on team members for the Premium plan'  
+  },
+  {
+    id:'2',
+    heading: 'What is the maximum file upload size?',
+    content: 'No more than 2GB. All files in your account must fit your allotted storage space.'  
+  },
+  {
+    id:'3',
+    heading: 'How do I reset my password?',
+    content: 'Click “Forgot password” from the login page or “Change password” from your profile page. A reset link will be emailed to you.'  
+  },
+  {
+    id:'4',
+    heading: 'Can I cancel my subscription?',
+    content: 'Yes! Send us a message and we’ll process your request no questions asked.'  
+  },
+  {
+    id:'5',
+    heading: 'Do you provide additional support?',
+    content: 'Chat and email support is available 24/7. Phone lines are open during normal business hours.'  
+  }
+]
